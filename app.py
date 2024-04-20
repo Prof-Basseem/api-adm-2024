4d84342ce9b2eaa4974c7d29c64d4dcda28ed545
Mahmoud Basseem <mahmoudbasseem@azhar.edu.eg>
2024-04-20 19:20:37 +0000

    Add static HTML and CSS files with other html files

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')  # You need to create an 'about.html'
+
+@app.route('/services')
+def services():
+    return render_template('services.html')  # You need to create a 'services.html'
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')  # You need to create a 'contact.html'
+
 if __name__ == '__main__':
     app.run(debug=True)
